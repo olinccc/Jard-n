2ca06b7b1a1bc44c7b3b035e5964dc6d1058aa15
olinca <olinca.laguna@gmail.com>
2025-09-19 23:02:13 -0600

    finAL FINAL

--- 01-Jueves-4-Sep/jardin 10/app.js
+++ 01-Jueves-4-Sep/jardin 10/app.js
@@ -36,31 +36,8 @@ function animate() {
 }
 animate();
 
-//Botón dinámico en JS
-const btn = document.createElement("button");
-btn.textContent = "girar";
-btn.style.position = "absolute";
-btn.style.top = "20px";
-btn.style.left = "20px";
-btn.style.padding = "10px 20px";
-btn.style.background = "#1e90ff";
-btn.style.color = "white";
-btn.style.border = "none";
-btn.style.borderRadius = "8px";
-btn.style.cursor = "pointer";
-btn.style.fontSize = "16px";
 
-btn.addEventListener("mouseenter", () => {
-  btn.style.background = "#104e8b";
-});
-btn.addEventListener("mouseleave", () => {
-  btn.style.background = "#1e90ff";
-});
-
-document.body.appendChild(btn);
-
-//Evento del botón con GSAP
-btn.addEventListener("click", () => {
+document.getElementById("btn").addEventListener("click", () => {
   gsap.to(cube.rotation, {
     duration: 2,
     y: cube.rotation.y + Math.PI * 4, 
@@ -68,9 +45,9 @@ btn.addEventListener("click", () => {
   });
 });
 
-
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
+
