5d31c375f03f873328798a3c9a49d3d23fced482
olinca <olinca.laguna@gmail.com>
2025-10-29 23:52:00 -0600

    jardin 19

---
+++ 01-Jueves-4-Sep/jardin 19 /app.js
@@ -0,0 +1,8 @@
+console.log("jardin 17 - canals");
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Configuración del lienzo
+    const lienzo = document.getElementById('lienzo');
+    console.log('Lienzo configurado: 1440x750px');
+
+});
